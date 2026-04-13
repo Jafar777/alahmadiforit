b9580ab8690e63fe5d942e2fb990cb1593352629
jafar <144897803+Jafar777@users.noreply.github.com>
2026-04-13 15:10:11 +0300

    go

--- app/layout.js
+++ app/layout.js
@@ -6,13 +6,17 @@ export const metadata = {
   description: 'Experience the future with Alahmadi Co',
   keywords: 'Alahmadi, الأحمدي, مؤسسة الأحمدي',
   authors: [{ name: 'Alahmadi Co' }],
-  viewport: 'width=device-width, initial-scale=1',
   robots: 'index, follow',
   openGraph: {
     title: 'مؤسسة الأحمدي | Alahmadi co',
     description: 'Official website of Alahmadi Co',
     type: 'website',
   },
+};
+
+export const viewport = {
+  width: 'device-width',
+  initialScale: 1,
 };
 
 export default function RootLayout({ children }) {
