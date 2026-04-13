65190e7dfee5b2d69058263caa696e969b5365eb
jafar <144897803+Jafar777@users.noreply.github.com>
2026-04-13 15:05:25 +0300

    sss

--- next.config.mjs
+++ next.config.mjs
@@ -1,11 +1,6 @@
-// next.config.js
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  // If you're using static export, remove this
-  output: 'standalone', // recommended for Vercel
-  images: {
-    unoptimized: true, // if you use next/image with external assets
-  },
-}
+  /* config options here */
+};
 
-module.exports = nextConfig
+export default nextConfig;
